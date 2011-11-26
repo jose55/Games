d216c2758df8433c6d890e569e58f2e224d7b9b9
Salepate <salepate@gmail.com>
2011-11-26 15:16:08 +0100

    Added first addon - GuiManager
    early version

---
+++ addons/src/guiManager.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+
+#include "guiManager.hpp"
+
+
+using namespace SGUI;
+using namespace std;
+
+
+/*
+*   SGUI::GUI
+*
+*/
+GUI::GUI() {
+    myShape = 0x00;
+
+}
+
+GUI::~GUI() {
+    for( std::vector<Widget*>::iterator it = myWidgets.begin(); it != myWidgets.end();) {
+        delete *it;
+        myWidgets.erase(it);
+    }
+}
+
+// SGUI::GUI::setPosition( float x, float y)
+void GUI::setPosition( float x, float y) {
+    myPos = sf::Vector2f(x, y);
+    mySprite.SetPosition(myPos);
+    if ( myShape ) myShape->SetPosition(x, y);
+}
+
+void GUI::setPosition( const sf::Vector2f & p) { setPosition(p.x, p.y); }
+
+// SGUI::GUI::setSize( float x, float y)
+void GUI::setSize( float x, float y) {
+    mySize = sf::Vector2f(x, y);
+    mySprite.SetSubRect( sf::IntRect(0, 0, x, y));
+}
+
+// SGUI::GUI::setImage( sf::Image * image)
+void GUI::setImage( sf::Image * image) {
+    mySprite.SetImage( *image );
+    mySprite.SetPosition(myPos);
+    mySize = sf::Vector2f( image->GetWidth(), image->GetHeight());
+}
+
+// SGUI::GUI::setShape( sf::Color col)
+void GUI::setShape( sf::Color col) {
+    if ( !myShape ) {
+        myShape = new sf::Shape;
+        *myShape = sf::Shape::Rectangle(myPos, myPos+mySize, sf::Color::White);
+    }
+    myShape->SetColor(col);
+}
+
+// SGUI::GUI::addWidget( SGUI::Widget * widget, const sf::Vector2f & p)
+void GUI::addWidget( SGUI::Widget * widget, const sf::Vector2f & p) {
+    widget->setPosition( myPos + p);
+    myWidgets.push_back(widget);
+}
+
+
+// SGUI::GUI::getButton( const sf::Vector2f & p)
+Widget * GUI::getWidget(const sf::Vector2f &p) {
+    for( std::vector<Widget*>::iterator it = myWidgets.begin(); it != myWidgets.end(); ++it) {
+        Widget * w = *it;
+        if ( p.x >= w->myPos.x &&
+             p.x <= w->myPos.x + w->mySize.x &&
+             p.y >= w->myPos.y &&
+             p.y <= w->myPos.y + w->mySize.y) return w;
+    }
+    return 0;
+}
+
+
+// SGUI::GUI::Render( sf::RenderTarget & target) const;
+void GUI::Render( sf::RenderTarget& target) const {
+    target.Draw(mySprite);
+    if ( myShape ) target.Draw(*myShape);
+    for( std::vector<Widget*>::const_iterator it = myWidgets.begin(); it != myWidgets.end(); ++it)
+        target.Draw(**it);
+}
+
+// SGUI::GUI::getPosition()
+const sf::Vector2f GUI::getPosition() { return myPos; }
+
+// SGUI::GUI::getSize()
+const sf::Vector2f GUI::getSize() { return mySize; }
+
+
+// SGUI::Alert::Alert()
+Alert::Alert() {
+    myShape = new sf::Shape();
+    *myShape = sf::Shape::Rectangle(0, 0, 100, 100, sf::Color(200, 0, 0, 70) );
+    myLength = 0.f;
+    myStatus = 0;
+}
+
+void Alert::setText( std::string text) {
+    myString.SetText(text);
+    myString.SetPosition(myPos+sf::Vector2f(2, 0));
+    myShape->SetPointPosition(1, myString.GetRect().Right-myString.GetRect().Left+4, 0);
+    myShape->SetPointPosition(2, myString.GetRect().Right-myString.GetRect().Left+4, myString.GetRect().Bottom);
+    myShape->SetPointPosition(3, 0, myString.GetRect().Bottom);
+}
+
+void Alert::Display() {
+    myClock.Reset();
+    myStatus = 1;
+}
+
+void Alert::Render( sf::RenderTarget & target) const {
+    if ( myStatus ) {
+        target.Draw(*myShape);
+        target.Draw(myString);
+    }
+}
+
+void Alert::Update() {
+    if ( myClock.GetElapsedTime() > 3.f ) myStatus = 0;
+
+}
+/*
+*   SGUI::Manager
+*
+*/
+Manager::Manager() {
+    myWidget = 0x00;
+    myShape = sf::Shape::Rectangle(0, 0, 1, 1, sf::Color(255, 255, 255, 40) );
+    myShape.SetBlendMode( sf::Blend::Add );
+}
+
+Manager::~Manager() {
+    for( std::vector<GUI*>::iterator it = myGUIs.begin(); it != myGUIs.end(); ) {
+        delete *it;
+        myGUIs.erase(it);
+    }
+}
+
+void Manager::Update( const sf::Vector2f mouse) {
+    myMouse = mouse;
+
+    for( std::vector<GUI*>::iterator it = myGUIs.begin(); it != myGUIs.end(); ++it)
+        (*it)->Update();
+
+    GUI * g;
+    Widget * w;
+
+    if ( (g =getGUI()) && (w = g->getWidget(myMouse)) ) {
+        if ( w != myWidget ) {;
+            myShape.SetPosition( w->getPosition() );
+            myShape.SetPointPosition(1, w->getSize().x, 0);
+            myShape.SetPointPosition(2, w->getSize().x, w->getSize().y);
+            myShape.SetPointPosition(3, 0, w->getSize().y);
+            myWidget = w;
+        }
+    }
+    else myWidget = 0x00;
+}
+
+void Manager::Render( sf::RenderTarget & target) const {
+    for( std::vector<GUI*>::const_iterator it = myGUIs.begin(); it != myGUIs.end(); ++it)
+        target.Draw(**it);
+
+    if ( myWidget ) target.Draw(myShape);
+}
+
+// SGUI::Manger::addGUI( SGUI::GUI * g)
+void Manager::addGUI( GUI * g) {
+    if ( !g ) return;
+    myGUIs.push_back(g);
+}
+
+// SGUI::Manager::onGUI()
+bool Manager::onGUI() {
+    if ( getGUI() ) return true;
+    return false;
+}
+
+
+// SGUI::Manager::getGUI()
+GUI * Manager::getGUI() {
+    for( std::vector<GUI*>::const_iterator it = myGUIs.begin(); it != myGUIs.end(); ++it) {
+        GUI * g = *it;
+        if ( myMouse.x >= g->myPos.x &&
+             myMouse.x <= g->myPos.x + g->mySize.x &&
+             myMouse.y >= g->myPos.y &&
+             myMouse.y <= g->myPos.y + g->mySize.y ) return g;
+    }
+    return 0;
+}
+
+
+// SGUI::Manager::onButton()
+bool Manager::onWidget() {
+    GUI * g = getGUI();
+    if ( !g ) return false;
+    Widget * w = g->getWidget(myMouse);
+    if ( !w ) return false;
+    return true;
+}
+
+//SGUI::Manager::getButton()
+Widget * Manager::getWidget() {
+    if ( !getGUI()) return 0;
+    return getGUI()->getWidget( myMouse);
+}
---
+++ addons/src/guiManager_Widget.cpp
@@ -0,0 +1,107 @@
+#include "guiManager.hpp"
+
+using namespace SGUI;
+using namespace std;
+
+//================================================================
+// SGUI::Widget
+//================================================================
+Widget::Widget() {
+    myPos = sf::Vector2f(0, 0);
+    mySize = myPos;
+    myValue = 0;
+}
+
+Widget::~Widget() {}
+
+//================================================================
+// SGUI::Label
+//================================================================
+Label::Label() {
+    myType = widget_label;
+    myText.SetSize( 16.f);
+}
+
+void Label::setPosition( const sf::Vector2f & p) {
+    myPos = p;
+    myText.SetPosition(p);
+}
+
+void Label::Render( sf::RenderTarget & target) const {
+    target.Draw(myText);
+}
+
+void Label::setText( std::string text) {
+    myText.SetText(text);
+    mySize.x = myText.GetRect().Right;
+    mySize.y = myText.GetRect().Bottom;
+}
+
+//================================================================
+// SGUI::Button
+//================================================================
+Button::Button( int return_value ) {
+    myShape = 0x00;
+    mySprite = 0x00;
+    setValue( return_value );
+    myType = widget_button;
+}
+
+Button::~Button() {
+    if ( myShape ) delete myShape;
+    //cout << "Button deleted successfuly";
+}
+
+// SGUI::Button::setPosition( float x, float y)
+void Button::setPosition( float x, float y) {
+    myPos = sf::Vector2f(x, y);
+    if ( myShape ) myShape->SetPosition(myPos);
+    if ( mySprite ) mySprite->SetPosition(myPos);
+}
+void Button::setPosition( const sf::Vector2f & p) { setPosition( p.x, p.y); }
+
+
+// SGUI:Button::setImage( sf::Image * image)
+void Button::setImage( sf::Image * image) {
+    if ( !mySprite ) mySprite = new sf::Sprite();
+    mySprite->SetImage( *image );
+    mySprite->SetPosition(myPos);
+    mySize = mySprite->GetSize();
+
+}
+
+
+// SGUI::Render( sf::RenderTarget & target) const
+void Button::Render( sf::RenderTarget & target) const {
+    if ( myShape ) target.Draw(*myShape);
+    if ( mySprite ) target.Draw(*mySprite);
+}
+
+//================================================================
+// SGUI::IntBox
+//================================================================
+
+IntBox::IntBox() {
+    myBox = sf::Shape::Rectangle(0, 0, 48, 24, sf::Color(0xC0, 0xC0, 0xC0), 1, sf::Color::White);
+    myValue = 800;
+    myText.SetText( "0" );
+    myText.SetSize( 16.f);
+    myType = widget_box;
+    mySize.x = 48;
+    mySize.y = 24;
+}
+
+void IntBox::Render( sf::RenderTarget & target) const {
+    target.Draw(myBox);
+    target.Draw(myText);
+}
+
+void IntBox::setPosition( const sf::Vector2f & p) {
+    myPos = p;
+    myBox.SetPosition(p);
+    myText.SetPosition(p + sf::Vector2f(4, 0));
+}
+
+void IntBox::setValue( int val) {
+
+}
